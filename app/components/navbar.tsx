831c4b09841a55931d599f8e1fadcfbadb276ea6
Dinh Thien Phan <dithienphan@gmail.com>
2023-06-07 20:42:40 +0200

    without css

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,25 +1,24 @@
-import NavStyles from '~/styles/navbar.css';
+import NavStyles from '~/styles/navbarStyles.css';
 
 export function Navbar () {
     return (
-      <body>
-        <nav>
-          
-          <div>
-            <ul id="navbar">
-              <li>
-                <a href="/home">Home</a>
-              </li>
-              <li>
-                <a href="/home/search">Search</a>
-              </li>
-              <li>
-                <a href="/home/create">Create</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </body>
+      <>
+      <nav id="navbar">
+        <div>
+          <ul id="items">
+            <li>
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/home/search">Search</a>
+            </li>
+            <li>
+              <a href="/home/create">Create</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      </>
     );
 }
 
