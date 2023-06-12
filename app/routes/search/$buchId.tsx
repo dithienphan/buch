cb6cb6f6f560e0901fa7d212927cfaefa14e162b
Dinh Thien Phan <dithienphan@gmail.com>
2023-06-12 13:00:39 +0200

    file structure for routing

---
+++ app/routes/search/$buchId.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function Detail() {
+  return (
+    <React.Fragment>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Detail
+      </Typography>
+    </React.Fragment>
+  );
+}
