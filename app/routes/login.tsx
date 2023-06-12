ead82123b70bf02456734a4a67557fce19375ce7
Dinh Thien Phan <dithienphan@gmail.com>
2023-06-12 12:34:08 +0200

    login route

---
+++ app/routes/login.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function Login() {
+  return (
+    <React.Fragment>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Login
+      </Typography>
+    </React.Fragment>
+  );
+}
