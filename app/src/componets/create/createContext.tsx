5c2a6ce16e08556fd8a4f55285850de5be727c84
Dinh Thien Phan <dithienphan@gmail.com>
2023-06-19 13:25:56 +0200

    context

---
+++ app/src/componets/create/createContext.tsx
@@ -0,0 +1,27 @@
+import React, { useState, createContext } from 'react'
+
+type TBuch = {
+  titel: string
+  untertitel: string
+}
+
+type BuchContextType = {
+  buch: TBuch | null
+  setBuch: React.Dispatch<React.SetStateAction<TBuch | null>>
+}
+
+type BuchContextProviderProps = {
+  children: React.ReactNode
+}
+
+// export const BuchContext = createContext<BuchContextType | null>(null)
+export const BuchContext = createContext({} as BuchContextType)
+
+export const BuchContextProvider = ({ children }: BuchContextProviderProps) => {
+  const [buch, setBuch] = useState<TBuch | null>(null)
+  return (
+    <BuchContext.Provider value={{ buch, setBuch }}>
+      {children}
+    </BuchContext.Provider>
+  )
+}
